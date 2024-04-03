352ea4082d0d9070ac622cc31e4a9918636e241a
Nasir Mustapha <teboy.com@gmail.com>
2024-04-03 13:29:06 +0100

    minor dashboard fixes and clean up

--- app/dashboard/controller.py
+++ app/dashboard/controller.py
@@ -4,10 +4,15 @@ from app.route_guard import auth_required
 
 from app.dashboard.model import *
 from app.dashboard.schema import *
+from app.unit.model import Unit
+from app.vehicle.model import Vehicle, Vehicleallocation
 
 bp = Blueprint('dashboard', __name__)
 
 @bp.get('/dashboard')
 @login_required
 def view_dashboard():
-    return render_template('dashboard.html')
+    unallocated = Vehicle.get_all()
+    allocated = Vehicleallocation.get_all()
+    units = Unit.get_all()
+    return render_template('dashboard.html', unallocated=unallocated, allocated=allocated, units=units)
